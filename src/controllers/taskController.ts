26e2507f0bf3b016591734a75b642a5e97df4ed2
idrisstdev <idriss.tameri@gmail.com>
2025-08-03 14:53:26 +0200

    feat: add db and query

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -1,7 +1,9 @@
-import { taskSchema } from "../schemas/taskSchema";
-import Task from "../models/taskModel";
+import { taskSchema, taskUpdateSchema } from "../schemas/taskSchema";
+import { TaskModel } from "../models/taskModel";
+import Task from "../types/taskType";
 import { Request, Response } from "express";
 import { v4 as uuidv4 } from "uuid";
+import mongoose from "mongoose";
 
 let tasks: Task[] = [
   {
@@ -12,49 +14,76 @@ let tasks: Task[] = [
   },
 ];
 
-export function getTasks(req: Request, res: Response) {
-  res.json(tasks);
+export async function getTasks(req: Request, res: Response) {
+  try {
+    const tasks = await TaskModel.find();
+    res.json(tasks);
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
 }
-export const createTask = (req: Request, res: Response) => {
+export const createTask = async (req: Request, res: Response) => {
   const parseResult = taskSchema.safeParse(req.body);
 
   if (!parseResult.success) {
     return res.status(400).json({ message: parseResult.error.message });
   }
 
-  const taskData = parseResult.data;
-
-  tasks.push({
-    id: uuidv4(),
-    title: taskData.title,
-    description: taskData.description,
-    completed: false,
-  });
-  res.send(`La tâche ${taskData.title} a bien été ajouté`);
-};
-
-export const getTaskById = (req: Request, res: Response) => {
-  const task = tasks.find((t) => t.id === req.params.id);
-  if (!task) return res.status(404).json({ message: "Task not found" });
-  res.status(201).json(task);
-};
-
-export const updateTask = (req: Request, res: Response) => {
-  const task = tasks.find((t) => t.id === req.params.id);
-  if (!task) return res.status(404).json({ message: "Task not found" });
-  const parseResult = taskSchema.safeParse(req.body);
-
-  if (!parseResult.success) {
-    return res.status(400).json({ message: parseResult.error.message });
+  try {
+    const task = new TaskModel(parseResult.data);
+    await task.save();
+    res.send(`La tâche ${task.title} a bien été ajouté`);
+  } catch (error) {
+    res.status(400).json({ message: error });
   }
-  task.title = parseResult.data.title;
-  task.description = parseResult.data.description;
-  task.completed = parseResult.data.completed;
-  res.json(task);
 };
-export const deleteTask = (req: Request, res: Response) => {
-  const task = tasks.find((t) => t.id === req.params.id);
-  if (!task) return res.status(404).json({ message: "Task not found" });
-  tasks = tasks.filter((t) => t.id !== task.id);
-  res.json({ message: "Tâche supprimé" });
+
+export const getTaskById = async (req: Request, res: Response) => {
+  try {
+    const taskId = new mongoose.Types.ObjectId(req.params.id);
+    if (!mongoose.Types.ObjectId.isValid(taskId)) {
+      return res.status(400).json({ message: "Invalid Task ID format" });
+    }
+    const task = await TaskModel.findById(taskId);
+    if (!task) return res.status(404).json({ message: "Task not found" });
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
+};
+
+export const updateTask = async (req: Request, res: Response) => {
+  try {
+    const parseResult = taskUpdateSchema.safeParse(req.body);
+    if (!parseResult.success) {
+      return res.status(400).json({ message: parseResult.error.message });
+    }
+    const taskId = new mongoose.Types.ObjectId(req.params.id);
+    if (!mongoose.Types.ObjectId.isValid(taskId)) {
+      return res
+        .status(400)
+        .json({ message: "Le format de l'id de la tâche est invalide" });
+    }
+    const taskUpdated = await TaskModel.findByIdAndUpdate(taskId, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    console.log(taskUpdated);
+    if (!taskUpdated)
+      return res.status(404).json({ message: "Task not found" });
+    res.json(taskUpdated);
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
+};
+
+export const deleteTask = async (req: Request, res: Response) => {
+  try {
+    const taskDeleted = await TaskModel.findByIdAndDelete(req.params.id);
+    if (!taskDeleted)
+      return res.status(404).json({ message: "Task not found" });
+    res.json({ message: "Tâche supprimé" });
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
 };
